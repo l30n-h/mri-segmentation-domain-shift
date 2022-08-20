6251b776e82e7cbb25e28693c67154ac9f06c9dc
Leon Hüber <leon.hueber@dw.com>
2022-08-20 19:05:20 +0200

    rshift live data used

--- analyze/analyze_activations_rshift.py
+++ analyze/analyze_activations_rshift.py
@@ -11,200 +11,150 @@ def get_kullback_liebler_divergence(P, Q):
     return torch.sum(P * torch.log(P / (Q + 1e-3)))
 
 from scipy.stats import wasserstein_distance
-def get_wasserstein_distance(P, Q):
+def get_representation_shift(act_ref, act_test):
     return torch.tensor([
-        wasserstein_distance(P[:, k], Q[:, k])
-        for k in range(P.shape[1])
+        wasserstein_distance(act_ref[:, k].numpy(), act_test[:, k].numpy())
+        for k in range(act_ref.shape[1])
     ]).mean()
 
-def generate_stats_compare(parameters_a, parameters_b):
-    for name in parameters_a.keys():
-        features_a = parameters_a[name].to(torch.float32)
-        features_b = parameters_b[name].to(torch.float32)
 
-        if len(features_a.shape) == 0:
-            continue
-        if len(features_a.shape) == 1:
-            features_a = torch.unsqueeze(features_a, 1)
-            features_b = torch.unsqueeze(features_b, 1)
-            #continue
+def get_rshift_stats(act_ref, act_test):
+    act_ref = act_ref[0 : act_test.shape[0]]
+    diff = act_ref - act_test
+    rshift = get_representation_shift(act_ref, act_test)
+    return {
+        'rshift': rshift.round(decimals=4).item(),
+        'ref_mean': act_ref.mean().item(),
+        'ref_std': act_ref.std().item(),
+        'test_mean': act_test.mean().item(),
+        'test_std': act_test.std().item(),
+        'diff_mean': diff.mean().item(),
+        'diff_std': diff.std().item(),
+        'diff_mean_abs': diff.abs().mean().item(),
+        'diff_std_abs': diff.abs().std().item()
+    }
 
-        diff = features_a - features_b
+def load_data_live(task, trainer, fold_train, epoch, layers, dataset_keys, batches_per_scan=64):
+    layers_set = set(layers)
+    def is_module_tracked(name, module):
+        return name in layers_set
 
-        rshift = get_wasserstein_distance(features_a.numpy(), features_b.numpy())
-
-        yield {
-            'name': name,
-            'rshift': rshift.round(decimals=4).item(),
-            'a_mean': features_a.mean().item(),
-            'a_std': features_a.std().item(),
-            'b_mean': features_b.mean().item(),
-            'b_std': features_b.std().item(),
-            'diff_mean': diff.mean().item(),
-            'diff_std': diff.std().item(),
-            'diff_mean_abs': diff.abs().mean().item(),
-            'diff_std_abs': diff.abs().std().item()
-        }
-
-def get_activations_dict(activations_paths, num_slices=None):
-    def get_slices_subset(activations_dict):
-        indices = torch.randint(
-            low=0,
-            high=next(iter(activations_dict.values())).shape[0],
-            size=(num_slices,)
+    def extract_activations(name, activations, slices_and_tiles_count, activations_dict):
+        activations_dict.setdefault(name, []).append(
+            activations[0].float().mean(dim=(1,2))
         )
+        return activations_dict
+
+    def merge_activations_dict(activations_dict):
         return dict(map(
             lambda item: (
                 item[0],
-                torch.index_select(
-                    item[1],
-                    0,
-                    indices
-                )
+                torch.stack(item[1]).cpu()
             ),
             activations_dict.items()
         ))
 
-    def get_slices_mean(activations_dict):
-        return dict(
-            map(
-                lambda x: (x[0], x[1].mean(dim=0, keepdim=True)),
-                activations_dict.items()
-            )
-        )
     
-    activations_dict_concat = dict()
-    for path in activations_paths:
-        activations_dict = torch.load(
-            path,
-            map_location=torch.device('cpu')
+    tmodel = hlp.load_model(trainer, fold_train, epoch)
+    hlp.apply_prediction_data_filter_monkey_patch(tmodel, batches_per_scan=batches_per_scan)
+
+    out = hlp.get_activations_dicts_merged([ activations_dict for id, prediction, activations_dict in hlp.generate_predictions_ram(
+        trainer=tmodel,
+        dataset_keys=dataset_keys,
+        activations_extractor_kwargs=dict(
+            extract_activations=extract_activations,
+            is_module_tracked=is_module_tracked,
+            merge_activations_dict=merge_activations_dict
         )
-        # TODO for each layer if not flattened and meaned than .flatten(2).mean(dim=2)
-        activations_dict = get_slices_mean(
-            activations_dict
-        ) if num_slices is None else get_slices_subset(
-            activations_dict
+    )])
+    return out
+
+def calc_rshift(task, trainers, folds, epochs, output_dir):
+    SCANS_PER_FOLD=24
+    BATCHES_PER_SCAN=64
+    LAYERS = hlp.get_layers_ordered()
+
+    def get_ids(scores, fold_test, is_validation):
+        return scores[
+            (scores['fold_test'] == fold_test) & (scores['is_validation'] == is_validation)
+        ].sort_values('id').head(SCANS_PER_FOLD)['id_long']
+    
+    def generate_rshift_stats_per_model(ids_train, folds_ids_dict_test, load_activations_means):
+        activations_ref_per_layer = load_activations_means(ids_train)
+
+        for (fold_test, is_validation), ids_test in folds_ids_dict_test.items():
+            activations_test_per_layer = load_activations_means(ids_test)
+
+            for layer, act_test in activations_test_per_layer.items():
+                yield {
+                    'fold_test': fold_test,
+                    'is_validation': is_validation,
+                    'name': layer,
+                    **get_rshift_stats(
+                        activations_ref_per_layer[layer],
+                        activations_test_per_layer[layer]
+                    )
+                }
+
+    output_dir = os.path.join(
+        output_dir,
+        '{}spf-{}bps'.format(
+            SCANS_PER_FOLD,
+            BATCHES_PER_SCAN,
         )
-
-        for key, value in activations_dict.items():
-            activations_dict_concat.setdefault(key, []).extend(
-                value
-            )
-
-    return dict(map(
-        lambda item: (
-            item[0],
-            torch.stack(item[1])
-        ),
-        activations_dict_concat.items()
-    ))
-
-def get_activations_id_path_map(directory_activations):
-    activation_paths = [ path for path in glob.iglob(
-        os.path.join(
-            directory_activations,
-            '*_activations.pkl'
-        ),
-        recursive=False
-    ) ]
-
-    return dict(map(
-        lambda path: (
-            hlp.get_id_from_filename(os.path.basename(path)),
-            path
-        ),
-        activation_paths
-    ))
-
-
-def calc_rshift():
-    task = 'Task601_cc359_all_training'
-    trainers = [
-        'nnUNetTrainerV2_MA_noscheduler_depth5_wd0_SGD_ep120__nnUNetPlansv2.1',
-        'nnUNetTrainerV2_MA_noscheduler_depth5_wd0_SGD_ep120_noDA__nnUNetPlansv2.1',
-        'nnUNetTrainerV2_MA_noscheduler_depth5_wd0_ep120__nnUNetPlansv2.1',
-        'nnUNetTrainerV2_MA_noscheduler_depth5_wd0_ep120_noDA__nnUNetPlansv2.1',
-    ]
-    folds = [
-        'siemens15',
-        'siemens3',
-        'ge15',
-        'ge3',
-        'philips15',
-        'philips3'
-    ]
-    epochs = [10,20,30,40,80,120] #[40]
-
-    output_dir = 'data/csv/activations-rshift'
+    )
     os.makedirs(output_dir, exist_ok=True)
 
     for trainer, fold_train, epoch in itertools.product(trainers, folds, epochs):
-        id_path_map = get_activations_id_path_map(
-            os.path.join(
-                hlp.get_testdata_dir(task, trainer, fold_train, epoch),
-                'activations-mean'
-            )
-        )
-        
         scores = hlp.get_scores(
             task,
             trainer,
             fold_train,
             epoch
         )
-
-        scores = scores[scores['id'].isin(set(id_path_map.keys()))]
-        scores['activations_path'] = scores['id'].apply(lambda x: id_path_map.get(x))
-
-        scores_train = scores[scores['fold_test'] == fold_train]
-        scores_train = scores_train[~scores_train['is_validation']]
-        #TODO more subsets could be used
-        activation_paths_train = scores_train.nlargest(
-            12,
-            'sdice_score'
-        )['activations_path'].tolist()
-        
-        
-        num_slices = 64
-        activations_ref = get_activations_dict(
-            activation_paths_train,
-            num_slices
-        )
-
-        for fold_test in folds:
-            print(trainer, fold_train, fold_test, epoch)
-
-            scores_test = scores[scores['fold_test'] == fold_test]
-            #TODO more subsets could be used but already overlaps with train but random slices used!!
-            scores_test = scores_test.sort_values('is_validation', ascending=False).head(12)
-
-            activation_paths_test = scores_test['activations_path'].tolist()
-
-            activations = get_activations_dict(
-                activation_paths_test,
-                num_slices
+        ids_train = get_ids(scores, fold_train, False)
+        folds_ids_dict_test = dict(map(
+            lambda key: (key, get_ids(scores, *key)),
+            itertools.product(
+                scores['fold_test'].unique(),
+                scores['is_validation'].unique()
             )
-            stats = pd.DataFrame(generate_stats_compare(
-                activations_ref,
-                activations
-            ))
-            stats['trainer'] = trainer
-            stats['fold_train'] = fold_train
-            stats['epoch'] = epoch
-            stats['fold_test'] = fold_test
-            print(stats)
-            stats.to_csv(
-                os.path.join(
-                    output_dir,
-                    'activations-rshift-{}-{}-{}-{}.csv'.format(trainer, fold_train, epoch, fold_test)
+        ))
+
+        stats = pd.DataFrame(generate_rshift_stats_per_model(
+            ids_train,
+            folds_ids_dict_test,
+            lambda keys: load_data_live(
+                task,
+                trainer,
+                fold_train,
+                epoch,
+                LAYERS,
+                keys,
+                batches_per_scan=BATCHES_PER_SCAN
+            )
+        ))
+        stats['task'] = task
+        stats['trainer'] = trainer
+        stats['fold_train'] = fold_train
+        stats['epoch'] = epoch
+        print(stats)
+        stats.to_csv(
+            os.path.join(
+                output_dir,
+                'activations-rshift-{}-{}-{}.csv'.format(
+                    trainer,
+                    fold_train,
+                    epoch
                 )
             )
+        )
 
 
 
-def plot_rshift():
+def plot_rshift(stats_path):
     stats = pd.concat([
-        pd.read_csv(path) for path in glob.iglob('data/csv/activations-rshift/activations-rshift-*.csv', recursive=False)
+        pd.read_csv(path) for path in glob.iglob(os.path.join(stats_path, '*'), recursive=False)
     ])
     stats['trainer_short'] = stats['trainer'].apply(hlp.get_trainer_short)
 
@@ -223,15 +173,15 @@ def plot_rshift():
     ).reset_index()
     stats = stats.join(scores.set_index(index), on=index)
 
-    layers_set = set(hlp.get_layers_ordered())
     layers_position_map = hlp.get_layers_position_map()
+    layers_position_map['input'] = -2
+    layers_position_map['gt'] = -1
     stats.rename(
         columns={
             'name': 'layer'
         },
         inplace=True
     )
-    stats = stats[stats['layer'].isin(layers_set)]
     stats['layer_pos'] = stats['layer'].apply(lambda d: layers_position_map.get(d))
     stats['same_fold'] = (stats['fold_train'] == stats['fold_test']).astype(int)
 
@@ -264,7 +214,10 @@ def plot_rshift():
     stats = stats[stats['epoch'] >= 40]
     #stats = stats[stats['epoch'] == 40]
 
-    output_dir = 'data/fig/activations-rshift'
+    output_dir = os.path.join(
+        'data/fig/activations-rshift',
+        os.path.basename(stats_path)
+    )
     os.makedirs(output_dir, exist_ok=True)
 
     for column in columns:
@@ -297,6 +250,30 @@ def plot_rshift():
 
 
 
-#calc_rshift()
+task = 'Task601_cc359_all_training'
+trainers = [
+    'nnUNetTrainerV2_MA_noscheduler_depth5_wd0_SGD_ep120__nnUNetPlansv2.1',
+    'nnUNetTrainerV2_MA_noscheduler_depth5_wd0_SGD_ep120_noDA__nnUNetPlansv2.1',
+    'nnUNetTrainerV2_MA_noscheduler_depth5_wd0_ep120__nnUNetPlansv2.1',
+    'nnUNetTrainerV2_MA_noscheduler_depth5_wd0_ep120_noDA__nnUNetPlansv2.1',
+]
+folds = [
+    'siemens15',
+    'siemens3',
+    'ge15',
+    'ge3',
+    #'philips15',
+    #'philips3'
+]
+epochs = [10,20,30,40,80,120] #[40]
 
-plot_rshift()
+#calc_rshift(task, trainers, folds, epochs, output_dir='data/csv/activations-rshift')
+
+plot_rshift('data/csv/activations-rshift/24spf-64bps')
+
+
+#TODO instnorm
+#  batchnorm makes use of train mean and std
+#  instancenorm shifts each slice to 0-mean 1-std (rsp beta-mean gamma-std)
+#  rshift paper uses resnet18/inception-v3/googlenet with bn and tracking stats
+#  btw unet2d used in lab also uses batchnorm
